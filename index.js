b13b78769f3a86b04321106e07d560d32688ac8f
NeosAz <zhenghaifeng27@gmail.com>
2023-06-08 23:09:45 -0400

    lastest update

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ const virtualBoard = (() => {
   return { placeSymbol, resetBoard, boardContent };
 })();
 
+
+
+
 const player = (__symbol) => {
   const symbol = __symbol;
   const score = 0;
@@ -21,9 +24,14 @@ const player = (__symbol) => {
   return { symbol, score };
 };
 
+
+
+
 const physicalBoard = (() => {
   const fields = document.querySelectorAll(".fields");
   const message = document.querySelector(".message");
+  const scoreBoard = document.querySelectorAll(".player_score");
+  const resetButton = document.querySelector(".reset");
 
   fields.forEach(function (field) {
     field.addEventListener("click", function () {
@@ -32,23 +40,40 @@ const physicalBoard = (() => {
     });
   });
 
+  resetButton.addEventListener("click", function(){
+    virtualBoard.resetBoard();
+    playerController.resetPlayerControl();
+    update();
+  })
+
   const update = () => {
+    let allowScoreUpdate = true
     for (let i = 0; i < 9; i++) {
       fields[i].textContent = virtualBoard.boardContent[i];
     }
-    message.textContent = playerController.message()
+
+
+    for (let i = 0; i < 2; i++) {
+      scoreBoard[i].textContent = playerController.player_score[i];
+    }
+    message.textContent = playerController.message();
   };
 })();
+
+
+
 
 const playerController = (() => {
   const player1 = player("X");
   const player2 = player("O");
+  const player_score = [];
+  let total_rounds = 0;
   let currentTurn = 0;
   let gameover = false;
   let winner = "";
 
   const playerSymbol = () => {
-    if (currentTurn % 2 === 0) {
+    if ((currentTurn + total_rounds) % 2 === 0)  {
       return player1.symbol;
     } else {
       return player2.symbol;
@@ -56,12 +81,14 @@ const playerController = (() => {
   };
 
   const placeSymbol = (index) => {
-    if (virtualBoard.boardContent[index] == "") {
+    if (virtualBoard.boardContent[index] == "" && gameover === false) {
       virtualBoard.placeSymbol(playerSymbol(), index);
       win(index);
       currentTurn++;
-      if (currentTurn === 9 && gameover === false) {
+      updateScore();
+      if (currentTurn === 9) {
         gameover = true;
+        total_rounds++;
         winner = "DRAW";
       }
     }
@@ -100,18 +127,38 @@ const playerController = (() => {
 
   const message = () => {
     let msg = "";
-    if ((gameover === false)) {
+    if (gameover === false) {
       msg = playerSymbol() + "'s TURN";
-    } else if ((gameover === true)) {
+    } 
+    else if (gameover === true) {
       if (winner === "DRAW") {
         msg = winner;
       } else {
         msg = winner + " WON";
       }
     }
-    console.log(msg);
     return msg;
   };
 
-  return {placeSymbol, message};
+  const updateScore = () => {
+    if (winner === "X") {
+      player1.score++;
+      total_rounds++;
+    } else if (winner === "O") {
+      player2.score++;
+      total_rounds++;
+    }
+
+    player_score[0] = player1.score;
+    player_score[1] = player2.score;
+  };
+
+  const resetPlayerControl = () => {
+    currentTurn = 0;
+    gameover = false;
+    winner = "";
+
+  };
+
+  return { placeSymbol, message, resetPlayerControl, player_score };
 })();
